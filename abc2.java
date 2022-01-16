c8bf9f1a4eaf7efc27d3d97a42ce47fca001665f
gunslinger147 <97821503+gunslinger147@users.noreply.github.com>
2022-01-16 20:36:03 +0530

    Add files via upload

---
+++ abc2.java
@@ -0,0 +1,10 @@
+package abcs;
+
+public class abc2 {
+    public static void main(String[] args) {
+        int a=9;
+        int b=12;
+
+        System.out.println("addition = "+(9+12));
+    }
+}
