1fc9fcb7b68ab2fd57526e637ce521c937e31a40
gunslinger147 <97821503+gunslinger147@users.noreply.github.com>
2022-01-16 21:08:08 +0530

    Update first.java

--- first.java
+++ first.java
@@ -3,5 +3,6 @@ package abcs;
 public class first {
     public static void main(String[] args) {
         System.out.println("this is first code");
+        System.out.println("Also new branch created");
     }
 }
